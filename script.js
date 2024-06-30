4977274dfdb9065a2c0e4842a6a9a4061c0e217f
Robodlan <reinaldo.robles@icloud.com>
2024-06-30 19:11:33 -0400

    update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const dino =  document.querySelector(".dino");
+   const grid =  document.querySelector(".grid");
+});
+
+function control() {
+
+};
+
+document.addEventListener("keyup", control);
