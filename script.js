0b54c5cdeb73ddf3f93fed03239a5e554f42a8c9
Robodlan <reinaldo.robles@icloud.com>
2024-06-30 19:38:55 -0400

    update scipt.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let position = 0;
+
 
 document.addEventListener("DOMContentLoaded", () => {
    const dino =  document.querySelector(".dino");
    const grid =  document.querySelector(".grid");
+   let gravity = 0.9;
 
    function control(e) {
     if (e.code === "Space") {
@@ -10,13 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
         jump()
     }
 }
-
+    let position = 0;
     function jump() {
+        let count = 0;
         let timerId = setInterval( () => {
-   
+         if (count === 15){
+            clearInterval(timerId);
+            let downTimer = setInterval( ()=> {
+                position -= 5;
+                position = position * gravity;
+                dino.style.bottom = position + 'px';
+            }, 20)
+         }
            // move up
            position += 30;
-           dino.style.bottom = position + 'px'
+           count++;
+           position = position * gravity;
+           dino.style.bottom = position + 'px';
         }, 20)
        };
 
