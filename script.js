8ec67c22fc71c7e14126d056b38913d7135e9352
Robodlan <reinaldo.robles@icloud.com>
2024-06-30 19:22:35 -0400

    functions

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+let position = 0;
+
 document.addEventListener("DOMContentLoaded", () => {
    const dino =  document.querySelector(".dino");
    const grid =  document.querySelector(".grid");
+
+   function control(e) {
+    if (e.code === "Space") {
+        console.log("jump");
+        jump()
+    }
+}
+
+    function jump() {
+        let timerId = setInterval( () => {
+   
+           // move up
+           position += 30;
+           dino.style.bottom = position + 'px'
+        }, 20)
+       };
+
+       document.addEventListener("keyup", control);
 });
 
-function control() {
 
-};
 
-document.addEventListener("keyup", control);
+    
+
+    
+
+
