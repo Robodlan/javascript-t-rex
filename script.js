ee8948fc413fecf0f34489fa36a0d3eb2abc7add
Robodlan <reinaldo.robles@icloud.com>
2024-06-30 19:52:51 -0400

    css

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
    const dino =  document.querySelector(".dino");
    const grid =  document.querySelector(".grid");
    let gravity = 0.9;
+   let isJumping = false;
 
    function control(e) {
     if (e.code === "Space") {
-        console.log("jump");
-        jump()
+        if (!isJumping) {
+            jump();
+        };
     }
 }
     let position = 0;
     function jump() {
+        isJumping = true;
         let count = 0;
         let timerId = setInterval( () => {
          if (count === 15){
             clearInterval(timerId);
             let downTimer = setInterval( ()=> {
+                if (count === 0) {
+                    clearInterval(downTimer);
+                    isJumping = false
+                }
                 position -= 5;
+                count--;
                 position = position * gravity;
                 dino.style.bottom = position + 'px';
             }, 20)
@@ -30,6 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
            dino.style.bottom = position + 'px';
         }, 20)
        };
+
+       function generateObstacles() {
+        
+       }
 
        document.addEventListener("keyup", control);
 });
